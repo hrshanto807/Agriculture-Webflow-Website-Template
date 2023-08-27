862781d6d2c67ff06bf3c793fef34815064a70b7
hrsanto <shantosss4907@gmail.com>
2023-08-27 21:57:13 +0600

    responsive max-width 768

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,51 @@
 (function ($) {
+    jQuery('#icon').click(function () { 
+        jQuery('.agr-primary-menu ul').slideToggle();       
+         
+     });
+     jQuery(window).resize(function () {
+         let windowSize = jQuery(window).width();
+         if (windowSize > 768) {
+             jQuery('.agr-primary-menu ul').removeAttr('style');
+         }
+     });
     $('.agr-testimonial-slider-area').owlCarousel({
         loop: true,
         margin: 10,
         nav: false,
-        items:1,
+        items: 1,
         dots: true,
-        autoplay:true,
-        autoplayTimeout:2000,
+        autoplay: true,
+        autoplayTimeout: 2000,
 
     });
     $('.agr-offer-area-slide').owlCarousel({
         loop: true,
         margin: 20,
-        nav: true,
-        items:4,
         dots: false,
-        autoplay:true,
-        autoplayTimeout:2000,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 2,
+                nav: true
+            },
+            1000: {
+                items: 4,
+                nav: true,
+                loop: false
+            }
+        }
+
         // navText: ['<i class="fa-solid fa-circle-arrow-left"></i>','<i class="fa-solid fa-circle-arrow-right"></i>'],
 
     });
+    
 
 
 })(jQuery);
---
+++ js/responsive.js
@@ -0,0 +1,3 @@
+if @media only screen and (max-width: 640px) {
+
+}
